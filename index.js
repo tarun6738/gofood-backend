eaf77942c7fff8990b821d12208e3f14febf9d06
tarun6738 <118709508+tarun6738@users.noreply.github.com>
2024-11-04 20:40:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const mongoDB = require('./db')
 mongoDB();
 
 app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin","https://gofood-frontend-three.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
